a16d13f661f71e609c10fd5060b925800d2bfeee
SeBin Nam <nsb2519@e-mirim.hs.kr>
2016-06-07 20:49:19 +0900

    #4 change into listDialogBox

--- app/src/main/java/kr/hs/emirim/sebin2519/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sebin2519/simpledialog/MainActivity.java
@@ -9,11 +9,13 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button but;
+    String[] items={"젤리빈", "킷켓", "롤리팝","마시멜로우"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button but=(Button)findViewById(R.id.but_dialog);
+        but=(Button)findViewById(R.id.but_dialog);
         but.setOnClickListener(this);//버튼이 클릭이 됫는지 안됫는지 감시
         //이벤트 핸들러 클래스(버튼을 눌렀을 때 동작, 처리하는 클래스)
     }
@@ -28,9 +30,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         AlertDialog.Builder dialog =new AlertDialog.Builder(this);//getApplicationContext(): 현재 엑티비티의 문맥을 반환 , 현재클래스를 반환
         dialog.setTitle("First Dialog");
-        dialog.setMessage("This is a message part!");
+        //dialog.setMessage("This is a message part!");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) { //which : 항목의 인덱스번호
+                but.setText(items[which]);
+            }
+        });//항목을 선택하면 익명클래스 실행
+
         dialog.setIcon(R.drawable.first_icon);
-        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+       dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(MainActivity.this, "대화상자의 확인 버튼을 클릭하셨습니다~!!", Toast.LENGTH_LONG).show(); //this -> 익명클래스 객체 this
