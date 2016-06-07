dd92e481b553f2e1555c4ff3addd9be03ce2e224
SeBin Nam <nsb2519@e-mirim.hs.kr>
2016-06-07 20:21:05 +0900

    #3 dialog.setPositiveButton에 Event Handler 설정

--- app/src/main/java/kr/hs/emirim/sebin2519/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sebin2519/simpledialog/MainActivity.java
@@ -1,10 +1,12 @@
 package kr.hs.emirim.sebin2519.simpledialog;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
@@ -24,11 +26,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
-        AlertDialog.Builder dialog =new AlertDialog.Builder(this);//getApplicationContext(): 현재 엑티비티의 문맥을 반환
+        AlertDialog.Builder dialog =new AlertDialog.Builder(this);//getApplicationContext(): 현재 엑티비티의 문맥을 반환 , 현재클래스를 반환
         dialog.setTitle("First Dialog");
         dialog.setMessage("This is a message part!");
         dialog.setIcon(R.drawable.first_icon);
-        dialog.setPositiveButton("확인",null);
+        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "대화상자의 확인 버튼을 클릭하셨습니다~!!", Toast.LENGTH_LONG).show(); //this -> 익명클래스 객체 this
+            }
+        }); //핸들러 자리, 익명클래스: 인터페이스로 상속받아 객체생성까지!
         dialog.show();
     }
 }
